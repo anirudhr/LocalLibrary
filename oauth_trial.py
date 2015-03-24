01be1cc285c50d758e63294d580d9885a15e6d3d
Anirudh Ramachandran <anirudhr1989@gmail.com>
2015-03-24 22:29:01 +0000

    Modularized oauth service

--- oauth_trial.py
+++ oauth_trial.py
@@ -2,25 +2,28 @@ from rauth.service import OAuth1Service, OAuth1Session
 from xml.dom.minidom import parseString
 import configparser
 
-config = configparser.ConfigParser()
-config.read("goodreads.cfg")
-CONSUMER_KEY = config.get("Goodreads", "CONSUMER_KEY")
-CONSUMER_SECRET = config.get("Goodreads", "CONSUMER_SECRET")
+def get_oauthservice():
+    config = configparser.ConfigParser()
+    config.read("goodreads.cfg")
+    CONSUMER_KEY = config.get("Goodreads", "CONSUMER_KEY")
+    CONSUMER_SECRET = config.get("Goodreads", "CONSUMER_SECRET")
 
-goodreads = OAuth1Service(
-    consumer_key=CONSUMER_KEY,
-    consumer_secret=CONSUMER_SECRET,
-    name='goodreads',
-    request_token_url='http://www.goodreads.com/oauth/request_token',
-    authorize_url='http://www.goodreads.com/oauth/authorize',
-    access_token_url='http://www.goodreads.com/oauth/access_token',
-    base_url='http://www.goodreads.com/'
-    )
+    gr = OAuth1Service(
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        name='goodreads',
+        request_token_url='http://www.goodreads.com/oauth/request_token',
+        authorize_url='http://www.goodreads.com/oauth/authorize',
+        access_token_url='http://www.goodreads.com/oauth/access_token',
+        base_url='http://www.goodreads.com/'
+        )
+    return gr
 
+goodreads = get_oauthservice()
 request_token, request_token_secret = goodreads.get_request_token(header_auth=True)
+authurl = goodreads.get_authorize_url(request_token)
 
-authorize_url = goodreads.get_authorize_url(request_token)
-print('Visit this URL in your browser: ' + authorize_url)
+print('Visit this URL in your browser: ' + authurl)
 accepted = 'n'
 while accepted.lower() == 'n':
     # you need to access the authorize_link via a browser,
